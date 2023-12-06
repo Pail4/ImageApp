e1c0c1971f2b152c7a665cbde789549e8bbc4327
HukakHePak <valsidalvonilb@outlook.com>
2023-12-06 10:47:13 +0400

    добавил класс

---
+++ ImageHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp
+{
+    internal class ImageHandler // осуществляет всю работу
+    {
+    }
+}
