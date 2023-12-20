9ca3b0f5c61ad4c6e3fb2e2d4959d12150494b3e
Pail4 <slavau2002@gmail.com>
2023-12-20 12:45:23 +0400

    TASK-2

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace ImageApp
         {
             InitializeComponent();
         }
+
+        private void LoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (BMP,JPG,PNG,GIF)|*.JPG;*.PNG;*.GIF, *.BMP";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //pictureBox.Image = new Bitmap(openFileDialog.FileName); <- ��  ������� �� ���������
+            }
+        }
     }
 }
